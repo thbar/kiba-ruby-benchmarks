dc75c50f96779d5a95f565301895553647610e41
Thibaut Barrère <thibaut.barrere@gmail.com>
2018-11-08 19:49:52 +0100

    Log filename used

--- etl/csv_source.rb
+++ etl/csv_source.rb
@@ -5,10 +5,12 @@ class CSVSource
     @filename = options.fetch(:filename)
     @csv_options = options.fetch(:csv_options)
     @limit = options.fetch(:limit, nil)
+    @logger = options.fetch(:logger)
   end
   
   def each
     file_row_index = 0
+    @logger.info "Opening #{@filename}"
     CSV.foreach(@filename, @csv_options) do |row|
       file_row_index += 1
       yield(row)
